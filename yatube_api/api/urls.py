111d6c3ed335f9f93b5ee11e7cbf017eaddac267
Евгений Екишев <edgar1148@yandex.ru>
2023-05-29 21:35:19 +0300

    9 sprint

--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -10,7 +10,7 @@ v1_router.register('posts', PostViewSet)
 v1_router.register('groups', GroupViewSet)
 v1_router.register('follow', FollowViewSet, basename='followers')
 v1_router.register(r'^posts/(?P<post_id>\d+)/comments',
-                CommentViewSet, basename='comments')
+                   CommentViewSet, basename='comments')
 
 urlpatterns = [
     path('v1/', include(v1_router.urls)),
