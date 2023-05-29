38680809ac6cede9a4ef0736455dc9d36ec9a965
Евгений Екишев <edgar1148@yandex.ru>
2023-05-29 16:21:06 +0300

    9 sprint

--- README.md
+++ README.md
@@ -1,2 +1,126 @@
-# api_final
-api final
+### Проект api_final_yatube
+
+
+## Описание
+Настроена API для социальной сети Yatube.
+Позволяет публикацию записей, комментарии, подписки на авторов.
+
+## Запуск проекта
+
+- Клонировать репозиторий
+- Установить и активировать виртуальное окружение
+
+```
+python -m venv venv
+```
+
+```
+source venv/Scripts/activate
+```
+
+- Установить зависимости requirements.txt
+
+```
+pip install -r requirements.txt
+```
+
+- Выполнить миграции:
+
+```
+python manage.py migrate
+```
+
+- Создать суперпользователя:
+
+```
+python manage.py createsuperuser
+```
+
+- Запустить проект:
+
+```
+python manage.py runserver
+```
+
+## Примеры работы с API для всех пользователей
+
+- Cписок всех публикаций
+```
+GET api/v1/posts/
+```
+- Получить публикацию по id
+```
+GET api/v1/posts/{id}/ 
+```
+- Получить список групп
+```
+GET api/v1/groups/
+```
+- Получить информацию о группе
+```
+GET api/v1/groups/{id}/
+```
+- Получить все комментарии к публикациям
+```
+GET api/v1/{post_id}/comments/
+```
+Получить комментарий по id
+```
+GET api/v1/{post_id}/comments/{id}/
+```
+
+## Примеры работы с API для авторизованных пользователей
+
+- Для создания публикации
+
+```
+POST /api/v1/posts/
+```
+
+- Ответ:
+
+```json
+{
+"text": "string",
+"image": "string",
+"group": 0
+}
+```
+
+- Обновление публикации:
+
+```
+PUT /api/v1/posts/{id}/
+```
+
+- Ответ:
+
+```json
+{
+"text": "string",
+"image": "string",
+"group": 0
+}
+```
+
+- Частичное обновление публикации:
+
+```
+PATCH /api/v1/posts/{id}/
+```
+
+Ответ:
+
+```json
+{
+"text": "string",
+"image": "string",
+"group": 0
+}
+```
+
+- Удаление публикации:
+
+```
+DEL /api/v1/posts/{id}/
+```
--- yatube_api/posts/models.py
+++ yatube_api/posts/models.py
@@ -4,23 +4,85 @@ from django.db import models
 User = get_user_model()
 
 
+class Group(models.Model):
+    title = models.CharField(
+        max_length=200,)
+    slug = models.SlugField(
+        unique=True,)
+    description = models.TextField()
+
+    class Meta:
+        ordering = ('id',)
+
+    def __str__(self):
+        return self.title
+
+
 class Post(models.Model):
     text = models.TextField()
-    pub_date = models.DateTimeField('Дата публикации', auto_now_add=True)
+    pub_date = models.DateTimeField(
+        'Дата публикации',
+        auto_now_add=True)
     author = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name='posts')
+        User,
+        on_delete=models.CASCADE,
+        related_name='posts',)
+    group = models.ForeignKey(
+        Group,
+        on_delete=models.SET_NULL,
+        blank=True,
+        null=True,
+        related_name='posts',)
     image = models.ImageField(
-        upload_to='posts/', null=True, blank=True)
+        upload_to='posts/',
+        blank=True,
+        null=True,)
+
+    class Meta:
+        ordering = ('id',)
 
     def __str__(self):
         return self.text
 
 
+class Follow(models.Model):
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='follower',)
+    following = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='following',)
+
+    class Meta:
+        constraints = (
+            models.UniqueConstraint(
+                fields=('user', 'following'),
+                name='unique_follow'
+            ),)
+
+
 class Comment(models.Model):
     author = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name='comments')
+        User,
+        on_delete=models.CASCADE,
+        related_name='comments',
+    )
     post = models.ForeignKey(
-        Post, on_delete=models.CASCADE, related_name='comments')
+        Post,
+        on_delete=models.CASCADE,
+        related_name='comments',
+    )
     text = models.TextField()
     created = models.DateTimeField(
-        'Дата добавления', auto_now_add=True, db_index=True)
+        'Дата добавления',
+        auto_now_add=True,
+        db_index=True
+    )
+
+    class Meta:
+        ordering = ('id',)
+
+    def __str__(self):
+        return self.text
